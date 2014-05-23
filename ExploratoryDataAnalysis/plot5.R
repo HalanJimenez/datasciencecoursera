13aa525ac4f3eaff8ee65eecdae8b7da2ce36b59
Halan Jimenez Gil <halan.jimenez@gmail.com>
2014-05-23 00:38:41 -0500

    Question 5 and a wrong Question 6 :P

--- ExploratoryDataAnalysis/plot5.R
+++ ExploratoryDataAnalysis/plot5.R
@@ -10,3 +10,18 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 ## 5.0 How have emissions from motor vehicle sources changed from 1999-2008 in
 ## Baltimore City?
+
+baltimore <- NEI[NEI$fips == "24510", ]
+sectors <- SCC[SCC$EI.Sector %in% c(grep("On-Road", unique(SCC$EI.Sector), value = TRUE)), ]
+vehicleBaltimore <- baltimore[baltimore$SCC %in% sectors$SCC, ]
+vehicleBaltimoreEmissions <- ddply(vehicleBaltimore, .(year), summarize, totalEmissions = as.numeric(format(round(sum(Emissions/1000), 2), nsmall = 2)))
+png("plot5.png", width = 500, height = 480)
+ggplot(vehicleBaltimoreEmissions, aes(x = vehicleBaltimoreEmissions$year, y = vehicleBaltimoreEmissions$totalEmissions)) +
+        ggtitle("Total Emissions in Baltimore from motor vehicle sources \n from 1999 to 2008") +
+        xlab("Year") +
+        ylab("Total Emissions (thousands of tons)") +
+        geom_point(aes(col = vehicleBaltimoreEmissions$totalEmissions)) +
+        geom_line(aes(col = vehicleBaltimoreEmissions$totalEmissions)) +
+        geom_text(aes(label = vehicleBaltimoreEmissions$totalEmissions, y = vehicleBaltimoreEmissions$totalEmissions), size = 3) +
+        theme(legend.position = 'none')
+dev.off()
--- ExploratoryDataAnalysis/plot6.R
+++ ExploratoryDataAnalysis/plot6.R
@@ -12,3 +12,22 @@ SCC <- readRDS("Source_Classification_Code.rds")
 ## emissions from motor vehicle sources in Los Angeles County, California
 ## (fips == "06037"). Which city has seen greater changes over time in motor
 ## vehicle emissions?
+
+baltimore <- NEI[NEI$fips == "24510", ]
+la <- NEI[NEI$fips == "06037",]
+sectors <- SCC[SCC$EI.Sector %in% c(grep("On-Road", unique(SCC$EI.Sector), value = TRUE)), ]
+vehicleBaltimore <- baltimore[baltimore$SCC %in% sectors$SCC, ]
+vehicleLA <- la[la$SCC %in% sectors$SCC, ]
+vehicleBaltimoreLA <- rbind(vehicleBaltimore, vehicleLA)
+vehicleBaltimoreLAEmissions <- ddply(vehicleBaltimoreLA, .(year, fips), summarize, totalEmissions = as.numeric(format(round(sum(Emissions), 2), nsmall = 2)))
+vehicleBaltimoreLAEmissions$fips <- factor(vehicleBaltimoreLAEmissions$fips, levels = c("06037", "24510"), 
+                                   labels = c("LA", "Baltimore"))
+png("plot6.png", width = 800, height = 480)
+ggplot(vehicleBaltimoreLAEmissions, aes(x = vehicleBaltimoreLAEmissions$year, y = vehicleBaltimoreLAEmissions$totalEmissions)) +
+        ggtitle("Total Emissions in Baltimore Vs Los Angeles from motor vehicle sources \n from 1999 to 2008") +
+        xlab("Year") +
+        ylab("Total Emissions (thousands of tons)") +
+        geom_bar(aes(col = vehicleBaltimoreLAEmissions$totalEmissions), stat = 'identity') +
+        geom_text(aes(label = vehicleBaltimoreLAEmissions$totalEmissions, y = vehicleBaltimoreLAEmissions$totalEmissions), size = 3) +
+        theme(legend.position = 'none')
+dev.off()
